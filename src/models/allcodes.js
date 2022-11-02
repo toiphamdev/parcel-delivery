622130f20a242a2ad9f56f7fe78d05f2c8fb3528
toiphamdev <toiphamasi79@gmail.com>
2022-11-02 13:08:43 +0700

    update admin page api

--- src/models/allcodes.js
+++ src/models/allcodes.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Allcode extends Model {
     /**
@@ -13,13 +11,17 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Allcode.init({
-    keyMap: DataTypes.TEXT,
-    type: DataTypes.TEXT,
-    value: DataTypes.TEXT,
-  }, {
-    sequelize,
-    modelName: 'Allcode',
-  });
+  Allcode.init(
+    {
+      keyMap: DataTypes.TEXT,
+      type: DataTypes.TEXT,
+      value: DataTypes.TEXT,
+      person: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Allcode',
+    }
+  );
   return Allcode;
 };
